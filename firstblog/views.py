aa9066c709fd3293f25490086e75570cd9225d0e
admin <williantagain@gmail.com>
2022-01-20 20:39:39 -0800

    Modification des templates to afficher les posts contenus dans la base de donnes.

--- firstblog/views.py
+++ firstblog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Firstblog
+
 def post_list(request):
- return render(request, 'firstblog/post_list.html', {})
+    firstblogs = Firstblog.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'firstblog/post_list.html', {'firstblogs': firstblogs})
